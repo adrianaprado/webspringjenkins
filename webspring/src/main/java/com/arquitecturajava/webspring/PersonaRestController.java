e679d1d3d93e17b1d9970d6949a15ce8684178b6
Adriana Prado <aprado@bilbomatica.es>
2024-11-13 11:15:03 +0100

    change Persona names to uppercase

--- webspring/src/main/java/com/arquitecturajava/webspring/PersonaRestController.java
+++ webspring/src/main/java/com/arquitecturajava/webspring/PersonaRestController.java
@@ -23,7 +23,7 @@ public class PersonaRestController {
 
 	@GetMapping
 	public List<PersonaDTO> buscarTodos() {
-		return servicio.buscarTodos().stream().map((p) -> new PersonaDTO(p.getNombre())).toList();
+		return servicio.buscarTodos().stream().map((p) -> new PersonaDTO(p.getNombre().toUpperCase())).toList();
 	}
 	
 }
