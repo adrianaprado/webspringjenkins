b72b1d1a2d86f92f77a56a3514e164f73b60f59f
Adriana Prado <aprado@bilbomatica.es>
2024-11-13 12:55:05 +0100

    Created junit test

--- webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
+++ webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
@@ -1,13 +1,23 @@
 package com.arquitecturajava.webspring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.arquitecturajava.webspring.models.Persona;
 
 @SpringBootTest
 class WebspringApplicationTests {
 
+	@Autowired
+	PersonaRepository personaRepository;
+	
 	@Test
-	void contextLoads() {
+	void addPersonaRepositoryTest() {
+		personaRepository.add(new Persona("Juan"));
+		assertEquals(personaRepository.buscarTodos().size(), 2);
 	}
 
 }
